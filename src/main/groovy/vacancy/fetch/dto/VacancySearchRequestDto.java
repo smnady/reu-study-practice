7cd48edf985c3086305541678a8b882ee3ee5910
smnadya <smirnovan5670@gmail.com>
2025-07-06 00:24:28 +0300

    delete unused

--- src/main/groovy/vacancy/fetch/dto/VacancySearchRequestDto.java
+++ src/main/groovy/vacancy/fetch/dto/VacancySearchRequestDto.java
@@ -8,46 +8,40 @@ import java.time.LocalDate;
 @Value
 @Builder
 public class VacancySearchRequestDto {
+
     /**
-     * Текст поиска (например, "Java")
+     * Текст поиска (например, "Java").
      */
     String query;
 
     /**
-     * Регион (например, "1" — Москва, "113" — Россия)
+     * Регион (например, "1" — Москва, "113" — Россия).
      */
     String area;
+
     /**
-     * Сколько на страницу (до 100)
+     * Сколько на страницу (до 100).
      */
     int perPage;
+
     /**
-     * максимум страниц (защитный лимит)
+     * Максимум страниц (защитный лимит).
      */
     int maxPages;
 
     /**
-     * дата "с"
+     * Дата "с".
      */
     LocalDate dateFrom;
+
     /**
-     * дата "по"
+     * Дата "по".
      */
     LocalDate dateTo;
+
     /**
-     * куда сохранять результат (можно null)
+     * Наименование файла для сохранения результата.
      */
     String fileName;
-    @Builder.Default
-    boolean saveId = true;
-    @Builder.Default
-    boolean saveTitle = true;
-    @Builder.Default
-    boolean saveEmployer = true;
-    @Builder.Default
-    boolean saveSnippet = false;      // snippet.requirement + snippet.responsibility
-    @Builder.Default
-    boolean saveAlternateUrl = true;
-    @Builder.Default
-    boolean saveKeySkills = false;
+
 }
